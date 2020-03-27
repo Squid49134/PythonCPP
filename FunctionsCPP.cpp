0e7229d64d4200fb24d5fd26a3297f9deeb018c6
Squid49134 <ransom@Udel.edu>
2020-03-27 12:58:23 -0400

    Added 2D array method and notes about how to build dll in VS

--- FunctionsCPP.cpp
+++ FunctionsCPP.cpp
@@ -1,6 +1,13 @@
 
-// Creating the library:
+// Creating the library via command line:
 //   g++ -o FunctionsCPP.so -shared FunctionsCPP.cpp
+
+// To create library in visual studio:
+//   Create dll project
+//   Change the build configuration to x64 NOTE THIS MUST BE DONE BEFORE DISABLING PRECOMPILED HEADERS OR YOU'll JUST HAVE TO DO IT AGAIN
+//   Disable precompiled headers, and delete all the nonsense VS creates like dllmain and pch.h make sure to delete both the headers and the source files
+//   Copy this file into a source file in VS
+//   Build the solution, and the dll will be placed in the x64/debug in the project folder
 
 
 // Functions to be added to the library, note extern "C" macro prevents the compiler from changing the function's name (which is done in C++ to handle overloading, but was not in C since overloading was not supported)
@@ -13,5 +20,13 @@ EXPORT double AddCPP(double num1, double num2) {
 EXPORT void MultByTwoCPP(double* numArr, unsigned int numElem) {
     for (int i = 0; i < numElem; i++) {
         numArr[i] = numArr[i] * 2.0;
+    }
+}
+
+EXPORT void MultByTwo2DCPP(double* numArr, unsigned int xDim, unsigned int yDim) {
+    for (int j = 0; j < yDim; j++) {
+        for (int i = 0; i < xDim; i++) {
+            numArr[j * xDim + i] = numArr[j * xDim + i] * 2.0;
+        }
     }
 }
--- WrapperCPP.py
+++ WrapperCPP.py
@@ -5,6 +5,7 @@ from numpy.ctypeslib import ndpointer
 
 # The path to the library file
 libPath   = "D:\\Kellie and Billy\\Documents\\Python\\C++ Wrapper\\FunctionsCPP.so"
+#libPath = "D:\\Kellie and Billy\\Documents\\Visual Studio 2019\\Projects\\ScatterAccel\\x64\\Debug\\ScatterAccel.dll"
 # importing the library
 library = ctypes.cdll.LoadLibrary(libPath)
 
@@ -18,6 +19,10 @@ def Add(num1, num2):
     # Calling the C++ function
     return func(num1, num2)
 
+print(Add(1.0, 1.0))
+print('')
+
+
 def MultByTwo(numArr):
     # Identifying the C++ function
     func          = library.MultByTwoCPP  # Name
@@ -29,11 +34,34 @@ def MultByTwo(numArr):
     
     return numArr
 
-
-
-print(Add(1.0, 1.0))
-
 array = np.zeros(5)
 for i in range(0, 5):
     array[i] = i + 1.0
 print(MultByTwo(array))
+print('')
+
+
+def MultByTwo2D(numArr, xDim, yDim):
+    # Identifying the C++ function
+    func          = library.MultByTwo2DCPP  # Name
+    func.restype  = int  # void return Type
+    func.argtypes = [ndpointer(ctypes.c_double), ctypes.c_uint, ctypes.c_uint]  # double pointer and 2 unsigned integer array dimensions to pass by reference
+    
+    # packing the 2D array into a 1D array to pass as simple pointer
+    packedArr = np.resize(array2D, (xDim * yDim))
+    # Calling the C++ function
+    func(packedArr, xDim, yDim)
+    # unpacking the returned 1D array into the original 2D dimmensions
+    unpackedArr = np.resize(packedArr, (yDim, xDim))
+    
+    return unpackedArr
+
+xDim = 3
+yDim = 4
+array2D = np.zeros((yDim, xDim))
+for j in range(0, yDim):
+    for i in range(0, xDim):
+        array2D[j][i] = j * xDim + i + 1
+print(MultByTwo2D(array2D, xDim, yDim))
+
+
